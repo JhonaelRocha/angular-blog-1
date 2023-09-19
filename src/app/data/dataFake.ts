d50f9a208095ec3f037f32b6bcbba87578f541e8
Jhonael <jhonaelrocha@gmail.com>
2023-09-19 10:30:46 -0300

    content criado

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "Novo Homem de Ferro",
+        "description": "Marvel studios anúncia novo filme do homem de ferro.",
+        "photoCover": "https://i.ytimg.com/vi/-kyYFnDMXMc/maxresdefault.jpg",
+    },
+    {
+        "id": "2",
+        "title": "Séries de sucesso da marvel.",
+        "description": "Séries de sucesso da marvel.",
+        "photoCover": "https://viciados.net/wp-content/uploads/2021/11/disney_day_2021_waifu2x_photo_noise3_scale_tta_1-1.webp",
+    },
+    {
+        "id": "3",
+        "title": "O novo filme do mario.",
+        "description": "",
+        "photoCover": "https://s2-techtudo.glbimg.com/_SNwRBXpexCnbUTYdqcvJ_AXpXw=/0x0:1200x700/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/c/u/JMc0NrTZGew6dSglJMSQ/mario-bros.jpg",
+    },
+    {
+        "id": "4",
+        "title": "O novo filme do sonic.",
+        "description": "",
+        "photoCover": "https://cinepop.com.br/wp-content/uploads/2023/01/sonic3_2.webp",
+    }
+]
